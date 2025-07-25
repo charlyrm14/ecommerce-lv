dd51b91ffabdef6fbce55be5b276d25afeac8641
charlyrm14 <charlyrm14@gmail.com>
2025-07-25 12:17:47 -0600

    feature: creacion de marca

---
+++ app/Resources/Brands/NewBrandResource.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Resources\Brands;
+
+use Carbon\Carbon;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class NewBrandResource extends JsonResource {
+
+    public function toArray($brand)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'status' => (bool) $this->status,
+            'created_at' => Carbon::parse($this->created_at)->format('Y-m-d'),
+            'updated_at' => Carbon::parse($this->updated_at)->format('Y-m-d'),
+        ];
+    } 
+}
